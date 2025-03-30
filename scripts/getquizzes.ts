f2aef5eeefebf108f051396be88e2cb3563d7452
Prince Diwakar <princediwakar25@gmail.com>
2025-03-30 09:12:29 +0530

    fixed sitemap generation & removed unnecessary script

--- scripts/getquizzes.ts
+++
@@ -1,9 +0,0 @@
-import { getQuizzes } from '@/app/temp-sitemap-quizzes';
-
-export async function testGetQuizzes() {
-  const page = 0; // Test with the first page
-  const quizzes = await getQuizzes(page);
-  console.log(`Test getQuizzes for page ${page}:`, quizzes);
-}
-
-testGetQuizzes();
